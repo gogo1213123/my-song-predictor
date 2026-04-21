21a670b2f2f0ac8e8e4c89dab376c33433e1a59b
gogo1213123 <18213157611@163.com>
2026-04-21 18:30:37 +0800

    Delete predict.py

--- predict.py
+++
@@ -1,6 +0,0 @@
-import joblib
-vectorizer, model = joblib.load("genre_predictor.pkl")
-new_song = input("晴天 周杰伦")#输入歌名和歌手
-X = vectorizer.transform([new_song])
-print(f"预测风格: {model.predict(X)[0]}")
-
